5f49e38b78fbc31f737265a334b217c8cabaefe2
artyombn <balabashinan@gmail.com>
2024-07-27 01:49:51 +0500

    Postgres

--- Brainwave/settings.py
+++ Brainwave/settings.py
@@ -82,13 +82,22 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Brainwave.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'brainwave',
+        'USER': 'artyombn',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
